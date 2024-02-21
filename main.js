7b757b4ff17d38845ad8e37e12b04c30829018af
T0nci <138928016+T0nci@users.noreply.github.com>
2024-02-21 17:37:48 +0100

    Add a function that displays the books
    Added function displays all books that the Library array contains with
    correct styling.

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+const books = document.querySelector(".books");
 const myLibrary = [];
 
+for (let i = 0; i < 4; i++) {
+    addBookToLibrary("example", "example", "1", true);
+}
 
-function Book(title = "/", author = "/", pages = 0, read = false) {
+showBooksOnPage();
+
+
+function Book(title = "/", author = "/", pages = "0", read = false) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -20,4 +27,35 @@ function Book(title = "/", author = "/", pages = 0, read = false) {
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.unshift(book);
+}
+
+
+function showBooksOnPage() {
+    for (const book of myLibrary) {
+        const title = document.createElement("div");
+        title.classList.add("title");
+        title.textContent = book.title;
+
+        const author = document.createElement("div");
+        author.classList.add("author");
+        author.textContent = "Author: " + book.author;
+
+        const pages = document.createElement("div");
+        pages.classList.add("pages");
+        pages.textContent = "Pages: " + book.pages;
+
+        const status = document.createElement("div");
+        status.classList.add("status");
+        if (book.read) status.textContent = "Read";
+        else status.textContent = "Not read";
+
+        const card = document.createElement("div");
+        card.classList.add("book");
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(status);
+
+        books.appendChild(card);
+    }
 }
